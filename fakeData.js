68419fa1911d8f1ba545d8114b110c1a04bba66d
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-06-12 16:49:39 -0300

    fix: added users to the fakeData

--- fakeData.js
+++ fakeData.js
@@ -1,29 +1,29 @@
 const fakeData = [
-  //   {
-  //     id: 1,
-  //     name: 'João Oliveira',
-  //     job: 'Desenvolvedor',
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Miguel Duarte',
-  //     job: 'Piloto',
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'Jessica Vasconcelos',
-  //     job: 'Oceonografa',
-  //   },
-  //   {
-  //     id: 4,
-  //     name: 'Alfredo Ricardo',
-  //     job: 'Bancário',
-  //   },
-  //   {
-  //     id: 5,
-  //     name: 'Ana Alves',
-  //     job: 'Empresária',
-  //   },
+  {
+    id: 1,
+    name: 'João Oliveira',
+    job: 'Desenvolvedor',
+  },
+  {
+    id: 2,
+    name: 'Miguel Duarte',
+    job: 'Piloto',
+  },
+  {
+    id: 3,
+    name: 'Jessica Vasconcelos',
+    job: 'Oceonografa',
+  },
+  {
+    id: 4,
+    name: 'Alfredo Ricardo',
+    job: 'Bancário',
+  },
+  {
+    id: 5,
+    name: 'Ana Alves',
+    job: 'Empresária',
+  },
 ]
 
 module.exports = fakeData
