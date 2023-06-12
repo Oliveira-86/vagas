f1fedba1494e657ae2980a1a4633d474c67f8e20
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-06-12 16:47:17 -0300

    core: added users to the data

--- fakeData.js
+++ fakeData.js
@@ -1,11 +1,29 @@
-
-const fakeData  =  [
-    {
-        id: 1,
-        name: "João Oliveira",
-        job: "Desenvolvedor"
-    }
+const fakeData = [
+  //   {
+  //     id: 1,
+  //     name: 'João Oliveira',
+  //     job: 'Desenvolvedor',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Miguel Duarte',
+  //     job: 'Piloto',
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Jessica Vasconcelos',
+  //     job: 'Oceonografa',
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Alfredo Ricardo',
+  //     job: 'Bancário',
+  //   },
+  //   {
+  //     id: 5,
+  //     name: 'Ana Alves',
+  //     job: 'Empresária',
+  //   },
 ]
 
-
-module.exports = fakeData;
+module.exports = fakeData
