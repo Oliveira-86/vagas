c5f945b172ff83364a5468102b656d83d2700817
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-06-12 17:03:45 -0300

    core: teste 2 done

--- teste2.js
+++ teste2.js
@@ -1,17 +1,23 @@
-var data =  require("./fakeData");
+var data = require('./fakeData')
 
-module.exports = function(req, res){
-  
-    var name =  req.body.name;
-    var jov =  req.body.job;
-    
-    var newUser = {
-        name: name,
-        job: job,
-    }
+module.exports = function (req, res) {
+  const { name, job } = req.body
 
-    data.push(newUser)
-    
-    res.send(newUser);
+  if (!name || !job) {
+    return res.status(400).json({ message: 'Passe os dados corretamente.' })
+  }
 
-};
+  const existingUser = data.find((user) => user.name === name)
+
+  if (existingUser)
+    return res.status(200).json({ message: 'Usuário já cadastrado.' })
+
+  const newUser = {
+    name,
+    job,
+  }
+
+  data.push(newUser)
+
+  res.status(200).json({ message: 'Usuário cadastrado com sucesso.' })
+}
