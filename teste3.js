4ce74d23a467efcdd47f2df785c1f79f0011b051
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-06-12 17:19:17 -0300

    core: teste3.js done

--- teste3.js
+++ teste3.js
@@ -1,15 +1,23 @@
-var data =  require("./fakeData");
+var data = require('./fakeData')
 
-module.exports = function(req, res) {
-  
-    var name =  req.query.name;
+module.exports = function (req, res) {
+  const { name } = req.body
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
-    }
+  if (!name) {
+    return res
+      .status(400)
+      .json({ message: 'Passe o nome do usuário que deseja deletar.' })
+  }
 
-    res.send("success");
+  const user = data.find((user) => user.name === name)
 
-};
+  if (!user) {
+    return res.status(404).json({ message: 'Usuário não encontrado.' })
+  }
+
+  const usersFiltered = data.filter((user) => user.name !== name)
+
+  return res
+    .status(200)
+    .json({ message: 'Usuário deletado com sucesso.', data: usersFiltered })
+}
